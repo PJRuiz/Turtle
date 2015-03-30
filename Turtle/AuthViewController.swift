a567f1d0974a00df9cb254dfbd23781bc2e3f0dc
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-03-30 21:09:52 +0900

    Added icons to AuthViewController and commented out UITabBar customisation

--- Turtle/AuthViewController.swift
+++ Turtle/AuthViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class AuthViewController: UIViewController {
     
+    @IBOutlet var emailTextField: UITextField?
+    
+    @IBOutlet var passwordTextField: UITextField?
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.edgesForExtendedLayout = UIRectEdge.None
         
+        var emailImageView = UIImageView(frame: CGRectMake(0,0,50, self.emailTextField!.frame.size.height))
+        emailImageView.image = UIImage(named: "EmailIcon")
+        emailImageView.contentMode = .Center
         
+        self.emailTextField!.leftView = emailImageView
+        self.emailTextField!.leftViewMode = .Always
+        
+        var passwordImageView = UIImageView(frame: CGRectMake(0,0,50, self.emailTextField!.frame.size.height))
+        passwordImageView.image = UIImage(named: "PasswordIcon")
+        passwordImageView.contentMode = .Center
+        
+        self.passwordTextField!.leftView = passwordImageView
+        self.passwordTextField!.leftViewMode = .Always        
         // Do any additional setup after loading the view.
     }
     
@@ -23,12 +40,20 @@ class AuthViewController: UIViewController {
     {
         super.viewWillAppear(animated)
         
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        self.navigationController!.setNavigationBarHidden(false, animated: false)
     }
+    
+    
+    // Make the keyboard appear automatically by using become first responder
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.emailTextField?.becomeFirstResponder()
+    }
+    
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.navigationController!.setNavigationBarHidden(true, animated: false)
     }
     
        override func didReceiveMemoryWarning() {
